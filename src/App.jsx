7834be7e498f4f6f156f50043365053f72be966a
Jaenudin Firdaus <2022110103@students.uigm.ac.id>
2025-03-26 08:55:03 +0700

    chore: configure react-query and react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="*" element={<h1>404 Not found</h1>} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools />
+        <App />
+      </QueryClientProvider>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
